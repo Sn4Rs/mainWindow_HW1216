9bb4c8b17cd2f98f3816e28844f6c8c58977b177
41243214 <wisbi1997@gmail.com>
2024-12-16 14:18:56 +0800

    add new shortcut keybinds

--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -29,9 +29,14 @@ public:
     QAction *actionSave;
     QAction *actionSave_As;
     QAction *actionQuit;
+    QAction *actionCut;
+    QAction *actionCopy;
+    QAction *actionPaste;
+    QAction *actionDelete;
     QWidget *centralwidget;
     QMenuBar *menubar;
     QMenu *menu;
+    QMenu *menu_2;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -51,6 +56,14 @@ public:
         actionSave_As->setObjectName("actionSave_As");
         actionQuit = new QAction(MainWindow);
         actionQuit->setObjectName("actionQuit");
+        actionCut = new QAction(MainWindow);
+        actionCut->setObjectName("actionCut");
+        actionCopy = new QAction(MainWindow);
+        actionCopy->setObjectName("actionCopy");
+        actionPaste = new QAction(MainWindow);
+        actionPaste->setObjectName("actionPaste");
+        actionDelete = new QAction(MainWindow);
+        actionDelete->setObjectName("actionDelete");
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         MainWindow->setCentralWidget(centralwidget);
@@ -59,12 +72,15 @@ public:
         menubar->setGeometry(QRect(0, 0, 800, 21));
         menu = new QMenu(menubar);
         menu->setObjectName("menu");
+        menu_2 = new QMenu(menubar);
+        menu_2->setObjectName("menu_2");
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
         MainWindow->setStatusBar(statusbar);
 
         menubar->addAction(menu->menuAction());
+        menubar->addAction(menu_2->menuAction());
         menu->addAction(actionNew);
         menu->addAction(actionOpen);
         menu->addAction(actionClose);
@@ -73,6 +89,10 @@ public:
         menu->addAction(actionSave_As);
         menu->addSeparator();
         menu->addAction(actionQuit);
+        menu_2->addAction(actionCut);
+        menu_2->addAction(actionCopy);
+        menu_2->addAction(actionPaste);
+        menu_2->addAction(actionDelete);
 
         retranslateUi(MainWindow);
 
@@ -82,13 +102,60 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        actionNew->setText(QCoreApplication::translate("MainWindow", "New", nullptr));
-        actionOpen->setText(QCoreApplication::translate("MainWindow", "Open", nullptr));
-        actionClose->setText(QCoreApplication::translate("MainWindow", "Close", nullptr));
-        actionSave->setText(QCoreApplication::translate("MainWindow", "Save", nullptr));
-        actionSave_As->setText(QCoreApplication::translate("MainWindow", "Save As", nullptr));
-        actionQuit->setText(QCoreApplication::translate("MainWindow", "Quit", nullptr));
+        actionNew->setText(QCoreApplication::translate("MainWindow", "New(&N)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionNew->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+N", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionOpen->setText(QCoreApplication::translate("MainWindow", "Open(&O)", nullptr));
+#if QT_CONFIG(tooltip)
+        actionOpen->setToolTip(QCoreApplication::translate("MainWindow", "Open Stuff", nullptr));
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        actionOpen->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+O", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionClose->setText(QCoreApplication::translate("MainWindow", "Close(ctrl+shift+C)", nullptr));
+#if QT_CONFIG(tooltip)
+        actionClose->setToolTip(QCoreApplication::translate("MainWindow", "Close stuff", nullptr));
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        actionClose->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+Shift+C", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionSave->setText(QCoreApplication::translate("MainWindow", "Save(&S)", nullptr));
+#if QT_CONFIG(tooltip)
+        actionSave->setToolTip(QCoreApplication::translate("MainWindow", "Save the thing", nullptr));
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        actionSave->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+S", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionSave_As->setText(QCoreApplication::translate("MainWindow", "Save As(&A)", nullptr));
+#if QT_CONFIG(tooltip)
+        actionSave_As->setToolTip(QCoreApplication::translate("MainWindow", "Save As New Thing", nullptr));
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        actionSave_As->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+A", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionQuit->setText(QCoreApplication::translate("MainWindow", "Quit(ctrl+shift+Q)", nullptr));
+#if QT_CONFIG(tooltip)
+        actionQuit->setToolTip(QCoreApplication::translate("MainWindow", "Quit This App", nullptr));
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        actionQuit->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+Shift+Q", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionCut->setText(QCoreApplication::translate("MainWindow", "Cut", nullptr));
+        actionCopy->setText(QCoreApplication::translate("MainWindow", "Copy(ctrl+C)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionCopy->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+C", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionPaste->setText(QCoreApplication::translate("MainWindow", "Paste(ctrl+V)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionPaste->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+V", nullptr));
+#endif // QT_CONFIG(shortcut)
+        actionDelete->setText(QCoreApplication::translate("MainWindow", "Delete(Del)", nullptr));
+#if QT_CONFIG(shortcut)
+        actionDelete->setShortcut(QCoreApplication::translate("MainWindow", "Del", nullptr));
+#endif // QT_CONFIG(shortcut)
         menu->setTitle(QCoreApplication::translate("MainWindow", "\346\252\224\346\241\210", nullptr));
+        menu_2->setTitle(QCoreApplication::translate("MainWindow", "\347\267\250\350\274\257", nullptr));
     } // retranslateUi
 
 };
